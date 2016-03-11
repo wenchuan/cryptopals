9b5f9d9118aa56e3c2850932e6346c6179817567
Wenchuan Weng <wengwenchuan@gmail.com>
2016-03-11 00:14:05 -0800

    implement repeating-key XOR

--- cryptopals/xor.py
+++ cryptopals/xor.py
@@ -2,3 +2,7 @@
 
 def xor(a,b):
     return [x^y for x,y in zip(a,b)]
+
+def xor_encrypt(cleartext, key):
+    '''key is an array'''
+    return [x^y for x,y in zip(cleartext, key * (len(cleartext) / len(key) + 1))]
--- tests/cryptopals_tests.py
+++ tests/cryptopals_tests.py
@@ -50,3 +50,10 @@ def test_base64_encode():
 
 def test_fixed_xor():
     assert base64.hex_decode('746865206b696420646f6e277420706c6179') == xor.xor(base64.hex_decode('1c0111001f010100061a024b53535009181c'), base64.hex_decode('686974207468652062756c6c277320657965'))
+
+def test_repeating_key_xor():
+    cleartext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
+    a = xor.xor_encrypt(map(ord, cleartext), map(ord, "ICE"))
+    b = base64.hex_decode('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
+    assert a == b
