dea51e1d02f739dab1c3a9f8c49667508504f2f5
Wenchuan Weng <wengwenchuan@gmail.com>
2016-03-10 23:33:50 -0800

    implement xor algorithm

---
+++ cryptopals/xor.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+def xor(a,b):
+    return [x^y for x,y in zip(a,b)]
--- tests/cryptopals_tests.py
+++ tests/cryptopals_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from nose.tools import *
-from cryptopals import base64
+from cryptopals import base64, xor
 from random import shuffle
 
 def setup():
@@ -47,3 +47,6 @@ def test_encode_block():
 
 def test_base64_encode():
     assert 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t' == base64.base64_encode('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+
+def test_fixed_xor():
+    assert base64.hex_decode('746865206b696420646f6e277420706c6179') == xor.xor(base64.hex_decode('1c0111001f010100061a024b53535009181c'), base64.hex_decode('686974207468652062756c6c277320657965'))
