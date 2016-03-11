09cd106cb937858f89e1bf513ccbc50591787133
Wenchuan Weng <wengwenchuan@gmail.com>
2016-03-11 00:13:01 -0800

    solve 'Detect single-character XOR'

---
+++ bin/detect_single_character_xor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#
+# http://cryptopals.com/sets/1/challenges/4/
+
+import requests
+from cryptopals import base64, xor
+
+r = requests.get('http://cryptopals.com/static/challenge-data/4.txt')
+
+ciphertexts = map(base64.hex_decode, r.text.split())
+
+def decrypt(key, ciphertext):
+    return xor.xor(ciphertext, [key] * len(ciphertext))
+
+def score_byte(b):
+    c = chr(b)
+    if str.isspace(c):
+        return 2
+    if str.isdigit(c):
+        return 1
+    if str.isalpha(c):
+        return 5
+    return 0
+
+def score(text):
+    return sum(map(score_byte, text))
+
+def solve():
+    scores = [(score(decrypt(k,c)), k, c) for k in xrange(256) for c in ciphertexts]
+    scores.sort(reverse=True)
+    print 'key | score | cleartext'
+    print '----+-------+----------'
+    for (s, k, c) in scores[:30]:
+        print format(k, '03d'), "|", format(s, '05d'), "|", ''.join(map(chr,decrypt(k,c)))
+
+if __name__ == '__main__':
+    solve()
